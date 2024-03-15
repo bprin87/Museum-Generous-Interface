9614f7f54861f96a42331d9b38bef00ae296f0ac
Ben <123553196+bprin87@users.noreply.github.com>
2024-03-15 13:38:37 +0000

    first commit

---
+++ Navigation.jsx
@@ -0,0 +1,15 @@
+import './Navigation.css';
+
+function Navigation() {
+
+    return (
+        <header>
+            <nav>
+                <button type='button'><a href='https://www.vam.ac.uk/' target='_blank'>Museum World Map</a></button>
+            </nav>
+        </header>
+    )
+
+}
+
+export default Navigation; 
