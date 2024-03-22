7258731e7da4ace411eaf521d8dcb21b9f9228dc
Ben <123553196+bprin87@users.noreply.github.com>
2024-03-22 14:16:00 +0000

    Added modal sidebar

---
+++ Modal.jsx
@@ -0,0 +1,21 @@
+import './Modal.css'
+import Artefact from './Artefact.jsx';
+
+function Modal() {
+
+    return (
+        
+        <section className="modal-container">
+            <h1>Vancouver</h1>
+            <h2>Canada</h2>
+            <Artefact/>
+            <Artefact/>
+             <Artefact/>
+            <Artefact/>
+        </section>
+        
+    )
+
+}
+
+export default Modal
