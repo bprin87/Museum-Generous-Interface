af81b893c57ec17ad1b9510462fb1b321bbcfb3a
Ben <123553196+bprin87@users.noreply.github.com>
2024-03-25 19:45:22 +0000

    Added clustering plugin
    Combines the total number of items in a location until zooming further into the map

--- Map.jsx
+++ Map.jsx
@@ -1,8 +1,11 @@
 import './Map.css'
 import {useRef, useState, useEffect} from 'react';
 import {MapContainer, TileLayer, ZoomControl, Marker, Tooltip} from "react-leaflet";
+import MarkerClusterGroup from 'react-leaflet-cluster';
+import 'react-leaflet-markercluster/dist/styles.min.css';
 import {setKey, fromAddress} from 'react-geocode';
 import "leaflet/dist/leaflet.css";
+
 
 function Map() {
 
@@ -32,6 +35,7 @@ function Map() {
 
             // loop through api data records
             for (const record of data.records) {
+
                 // use geocode to get coordinates of primaryPlace which is the
                 // original location of each item
                 if(record._primaryPlace) {
@@ -63,6 +67,8 @@ function Map() {
     const mapRef = useRef(null);
     const latitutude = 20;
     const longitude = 0;
+
+   
     
     return (
 
@@ -70,12 +76,14 @@ function Map() {
        <MapContainer className='map-container' data-testid='map-container' center={[latitutude, longitude]} zoom={2} minZoom={2} maxZoom={18} zoomControl={false} ref={mapRef}>
             <TileLayer attribution='attribution: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>' url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'/>
             <ZoomControl position='bottomright'/>
+            <MarkerClusterGroup maxClusterRadius={30} disableClusteringAtZoom={12}>
             {items.map((item, index) => (item.coordinates && (
                 <Marker key={index} position={[item.coordinates.lat, item.coordinates.lng]}>
                     <Tooltip direction='right' offset={[0, 0]} permanent>{locationTotal[item._primaryPlace]}</Tooltip>
                 </Marker>
             )
             ))}
+            </MarkerClusterGroup>
        </MapContainer>
        
     )
